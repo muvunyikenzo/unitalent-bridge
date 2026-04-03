50cb5727907897dbb5f3b8f7a886c3a4af944a24
muvunyikenzo <mkenzokelly@gmail.com>
2026-04-03 12:13:12 -0700

    "update user route"

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -3,11 +3,13 @@ import prisma from '@/lib/db';
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
+    const { id } = await params;
+    
     const user = await prisma.user.findUnique({
-      where: { id: params.id },
+      where: { id },
       select: {
         id: true,
         name: true,
